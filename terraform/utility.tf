b4b39b52e77bb31ce4f752c55fe003c89cf9f3c1
Antonio Cascais <acascais@indocresearch.org>
2025-08-11 11:08:34 +0200

    IEBH-438: add project service to utility namespace
    Added project service deployment with:
    - Project service Helm chart configuration (values.yaml)
    - Database credentials mapping from postgres secret
    - MinIO credentials mapping from minio secret
    - S3 integration with project logos bucket
    - Terraform variables for project chart and app versions

--- terraform/utility.tf
+++ terraform/utility.tf
@@ -71,6 +71,65 @@ resource "kubernetes_secret" "rsa_public_key" {
   }
 }
 
+# Read the existing postgres secret
+data "kubernetes_secret" "postgres_credential" {
+  metadata {
+    name      = "postgres"
+    namespace = kubernetes_namespace.utility.metadata[0].name
+  }
+  depends_on = [helm_release.postgres]
+}
+
+# Read the existing minio secret
+data "kubernetes_secret" "minio_credential" {
+  metadata {
+    name      = "minio"
+    namespace = "minio"
+  }
+}
+
+resource "kubernetes_secret" "opsdb_utility_credential" {
+  metadata {
+    name      = "opsdb-utility-credential"
+    namespace = kubernetes_namespace.utility.metadata[0].name
+  }
+
+  type = "Opaque"
+
+  data = {
+    "username" = "postgres"
+    "password" = data.kubernetes_secret.postgres_credential.data["postgres-password"]
+    "host"     = "postgres.utility"
+    "port"     = "5432"
+  }
+
+  lifecycle {
+    ignore_changes = [data]
+  }
+
+  depends_on = [data.kubernetes_secret.postgres_credential]
+}
+
+resource "kubernetes_secret" "minio_credential" {
+  metadata {
+    name      = "minio-credential"
+    namespace = kubernetes_namespace.utility.metadata[0].name
+  }
+
+  type = "Opaque"
+
+  data = {
+    "MINIO_ACCESS_KEY" = data.kubernetes_secret.minio_credential.data["root-user"]
+    "MINIO_SECRET_KEY" = data.kubernetes_secret.minio_credential.data["root-password"]
+  }
+
+  lifecycle {
+    ignore_changes = [data]
+  }
+
+  depends_on = [data.kubernetes_secret.minio_credential]
+}
+
 resource "kubernetes_config_map" "postgres_init" {
   metadata {
     name      = "postgres-init-scripts"
@@ -176,4 +235,33 @@ resource "kubernetes_manifest" "configmap_utility_kafka_plugin_downloader" {
       "namespace" = "utility"
     }
   }
+}
+
+resource "helm_release" "project" {
+
+  name = "project-service"
+
+  repository = "https://pilotdataplatform.github.io/helm-charts/"
+  chart      = "project-service"
+  version    = var.project_chart_version
+  namespace  = kubernetes_namespace.utility.metadata[0].name
+  timeout    = "300"
+
+  values = [file("../helm_charts/pilot-hdc/project/values.yaml")]
+
+  set {
+    name  = "image.tag"
+    value = var.project_app_version
+  }
+
+  set {
+    name  = "imagePullSecrets[0].name"
+    value = kubernetes_secret.docker_registry.metadata[0].name
+  }
+
+  depends_on = [
+    helm_release.postgres,
+    kubernetes_secret.opsdb_utility_credential,
+    kubernetes_secret.minio_credential
+  ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,6 +39,26 @@ variable "metadata_app_version" {
   default = "2.2.9"
 }
 
+variable "project_chart_version" {
+  type    = string
+  default = "0.2.1"
+}
+
+variable "project_app_version" {
+  type    = string
+  default = "2.3.7"
+}
+
+variable "dataops_chart_version" {
+  type    = string
+  default = "0.2.1"
+}
+
+variable "dataops_app_version" {
+  type    = string
+  default = "2.5.7"
+}
+
 variable "docker_registry_username" {
   type        = string
   description = "Username for docker-registry.ebrains.eu"
